b2d7637ca821bef24457b6b593b00ebdb2c2d778
itsjlove <65690844+itsjlove@users.noreply.github.com>
2020-07-30 13:34:25 -0400

    Create consultation.php

---
+++ mischasmirror/consultation.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html >
+      <html>
+      <head>
+        <meta charset="utf-8" >
+         <title>Thank you for your order</title>
+         <style> body {  background-color: #BDDEDD;  margin: 0 10%;  font-family: sans-serif;  }h1 {  text-align: center;  font-family: serif;  font-weight: normal;  text-transform: uppercase;  border-bottom: 1px solid #E68411;  margin-top: 30px;}
+          h2 {  color: #d1633c;  font-size: 1em;}
+            .topnav
+            {overflow: hidden;background-color: #BDDEDD;}
+            .topnav a
+            {float: left; color: #000000; text-align: center; padding: 14px 16px; text-decoration: none;font-size: 17px;}
+            .topnav a:hover
+            {background-color: #f9c453; color: black;}
+            .topnav a.active
+            {background-color: #f78121; color:#00000;}
+            footer {border-top:1px solid #57b1dc; text-align:center; padding-top: 1em; }
+            footer img {width:40px; height:40px; margin-left:.5em; margin-right: .5em;}
+          </style>
+      </head>
+      <body>
+        <h1>Thanks for your money!</h1>
+      <p>Thank you for ordering from Black Goose Bistro. We have received the following information about your order:</p>
+      <h2>Your Info</h2>
+      <ul>
+      <li><strong>Name:</strong> <em>Kyle</em></li>
+      <li><strong>Address:</strong> <em>33rd Street, Orlando, Florida</em></li>
+      <li><strong>Telephone number:</strong> <em>830-476-5664</em></li>
+      <li><strong>Email Address:</strong> <em>KyL31sSuP3rCo0L@hotmail.com</em></li>
+      </ul>
+      <p><strong>Delivery instructions:</strong> <em>The house that smells a little disappointment</em></p>
+      <h2>Your Carbs</h2>
+      <em>Sorry, your wifi shut off and we coudn't process your order. <a href="https://itsjlove.github.io/fid/blackgoose/order.html">Do it all over.</a></em>
+      <p class="disclaimer"><small>This site is for educational purposes only. No pizzas will show up at your exes.</small></p>
+      </body>
+      </html>
